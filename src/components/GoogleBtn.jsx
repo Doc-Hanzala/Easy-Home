61196d0818d24605fb104f75ddd38653e413f447
hanzala tanveer <810hanzalatanveer@gmail.com>
2024-01-02 12:05:52 -0800

    Worked with google sign up

--- src/components/GoogleBtn.jsx
+++ src/components/GoogleBtn.jsx
@@ -1,12 +1,41 @@
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
 import { FcGoogle } from "react-icons/fc";
+import { db } from "../firebase";
+import { toast } from "react-toastify";
 
 const GoogleBtn = () => {
+  const onClick = async () => {
+    try {
+      const provider = new GoogleAuthProvider();
+      const auth = getAuth();
+
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+
+      if (!docSnap.exists()) {
+        await setDoc(docRef, {
+          name: user.displayName,
+          email: user.email,
+          timeStamp: serverTimestamp(),
+        });
+      }
+    } catch (error) {
+      toast.error("Something went fishy with google authentication");
+    }
+  };
+
   return (
     <button
+      onClick={onClick}
       className="bg-slate-500 w-full rounded text-xl text-white uppercase py-2  hover:bg-slate-600 active:bg-green:700 transition-all my-3 shadow-md hover:shadow-lg flex justify-center items-center "
-      type="submit"
+      type="button"
     >
-      <FcGoogle className="text-2xl mx-3 bg-white rounded-full" /> continue with google
+      <FcGoogle className="text-2xl mx-3 bg-white rounded-full" /> continue with
+      google
     </button>
   );
 };
